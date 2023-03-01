8f4fadab1848a87f11e5ab1d926dc28d50149676
wlxlatiao <wlxlatiao@163.com>
2023-03-01 16:57:59 +0800

    3.1

--- graph_matching_test.py
+++ graph_matching_test.py
@@ -1,5 +1,5 @@
 # test graph matching
-import torch
+import numpy as np
 import pickle
 from tqdm import tqdm
 import pygmtools as pygm
@@ -18,17 +18,21 @@ def sim_graphs(g1, g2, thres):
 
 
 def match_graphs(g1, g2):
+    node1 = g1["labels"]
+    node2 = g2["labels"]
+    A1 = np.zeros((node1.shape[0], node1.shape[0]))
+    A2 = np.zeros((node2.shape[0], node2.shape[0]))
+
     pass
 
 
-pygm.BACKEND = 'pytorch'
-_ = torch.manual_seed(1)
-
+pygm.BACKEND = 'numpy'
+np.random.seed(1)
 threshold = 0.5
 
 path = "em_E.pk"
 l = pickle.load(open(path, "rb"))
-
+graph11 = l[0]["labels"]
 for i, graph1 in tqdm(enumerate(l)):
     for graph2 in l[i + 1:]:
         if sim_graphs(graph1, graph2, threshold):
