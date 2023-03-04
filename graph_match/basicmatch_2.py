34b48b871bd39f6a7da881fc7f8f982737191fc3
wlx_glb <wlxlatiao_glb@163.com>
2023-03-04 19:35:48 +0800

    3.4

--- graph_match/basicmatch_2.py
+++ graph_match/basicmatch_2.py
@@ -15,11 +15,12 @@ lb2idx = {k: int(v) for k, v in vocab["label_to_idx"].items()}
 idx2pred = {int(k): v for k, v in vocab["idx_to_predicate"].items()}
 pred2idx = {k: int(v) for k, v in vocab["predicate_to_idx"].items()}
 len_lb = len(idx2lb)
+len_pred = len(idx2pred)
 
 l = pickle.load(open(path, "rb"))
 
 num_node_features = 10
-num_edge_features = 100
+num_edge_features = 10
 sentence = []
 index = 0
 
@@ -39,7 +40,7 @@ model = Word2Vec(sentences=sentence, vector_size=num_edge_features, window=2, mi
 
 predfeatures = []
 for item in idx2pred:
-    predfeatures.append(model.wv[idx2pred[item]])
+    predfeatures.append(list(model.wv[idx2pred[item]]))
 
 graph1 = l[14]
 graph2 = l[165]
@@ -51,23 +52,42 @@ node1 = node1.T
 node2 = graph2["labels"] / len_lb
 node2 = np.atleast_2d(node2)
 node2 = node2.T
-
-A1 = np.zeros((node1.shape[0], node1.shape[0], num_edge_features))
-A2 = np.zeros((node2.shape[0], node2.shape[0], num_edge_features))
 n1 = np.array([node1.shape[0]])
 n2 = np.array([node2.shape[0]])
 
+# A1 = np.zeros((node1.shape[0], node1.shape[0]))
+# A2 = np.zeros((node2.shape[0], node2.shape[0]))
+# n1 = np.array([node1.shape[0]])
+# n2 = np.array([node2.shape[0]])
+#
+# for triple in graph1["relations"]:
+#     A1[triple[0]][triple[1]] = triple[2] / len_pred
+#
+# for triple in graph2["relations"]:
+#     A2[triple[0]][triple[1]] = triple[2] / len_pred
+#
+# conn1, edge1 = pygm.utils.dense_to_sparse(A1)
+# conn2, edge2 = pygm.utils.dense_to_sparse(A2)
+
+conn1 = []
+edge1 = []
+conn2 = []
+edge2 = []
 for triple in graph1["relations"]:
-    A1[triple[0]][triple[1]] = predfeatures[triple[2] - 1]
-
+    conn1.append([triple[0], triple[1]])
+    edge1.append(predfeatures[triple[2] - 1])
 for triple in graph2["relations"]:
-    A2[triple[0]][triple[1]] = predfeatures[triple[2] - 1]
-
-conn1, edge1 = pygm.utils.dense_to_sparse(A1)
-conn2, edge2 = pygm.utils.dense_to_sparse(A2)
+    conn2.append([triple[0], triple[1]])
+    edge2.append(predfeatures[triple[2] - 1])
+conn1 = np.array(conn1)
+conn2 = np.array(conn2)
+edge1 = np.array(edge1)
+edge2 = np.array(edge2)
+# ne1 = len(edge1)
+# ne2 = len(edge2)
 import functools
 
-gaussian_aff = functools.partial(pygm.utils.gaussian_aff_fn, sigma=1.)  # set affinity function
+gaussian_aff = functools.partial(pygm.utils.gaussian_aff_fn, sigma=10.)  # set affinity function
 K = pygm.utils.build_aff_mat(node1, edge1, conn1, node2, edge2, conn2, n1, None, n2, None, edge_aff_fn=gaussian_aff)
 
 X = pygm.rrwm(K, n1, n2)
